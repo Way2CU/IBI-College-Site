c2ad4d30a50517ce7e973e7b2d7145c375dab75b
iliya <iliyuskeisar@gmail.com>
2016-08-01 09:37:20 +0300

    script to controll the animation on scroll

---
+++ site/scripts/position_animation.js
@@ -0,0 +1,40 @@
+// create or use existing site scope
+var Site = Site || {};
+
+Site.PositionAnimation = function(elements) {
+	var self = this;
+
+	self.elements = Array.from(document.querySelectorAll(elements));
+
+	/*
+	 * object initialization 
+	 */
+	self._init = function() {
+		window.addEventListener('scroll', self.scroll_event);
+	};
+
+	self.scroll_event = function(event) {
+		if (self.elements.length == 0)
+			return;
+
+		// show list items
+		var to_show = new Array();
+
+		self.elements.forEach(function(element) {
+			if (window.scrollY >= element.offsetTop - element.clientHeight) 
+				element.classList.add('active'); else
+				to_show.push(element);
+		});
+
+		// replace item list with new one
+		self.elements = to_show;
+	};
+
+	// finish object initialization
+	self._init();
+}
+
+$(function() {
+	Site.scroll_features = new Site.PositionAnimation('section#features ul li');
+	Site.scroll1 = new Site.PositionAnimation('body article');
+});
